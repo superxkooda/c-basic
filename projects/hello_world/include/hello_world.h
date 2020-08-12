4f74b2e22a34da4fb0a82a02a8eb02966a791624
Michael Anghelone <superxkooda@gmail.com>
2020-08-11 20:10:35 -0400

    Simplified the hello world project
    No need to have seperate header and file for such a simple project.

--- projects/hello_world/include/hello_world.h
+++
@@ -1,6 +0,0 @@
-#ifndef HELLO_WORLD_H
-#define HELLO_WORLD_H
-
-int hello_world(void);
-
-#endif
--- projects/hello_world/src/main.c
+++ projects/hello_world/src/main.c
@@ -1,10 +1,20 @@
-#include "hello_world.h"
+#include <stdio.h>
 /*
- * Function: main
- * Run the main routine of this program. The primary focuse in the project is to execute our hello_world function
- * */
+ * Title: Hello World
+ * This is the most basic of programs.
+ * 
+ * Function: Main
+ * Every C program should have a main function unless a userdefined __start function is defined.
+ * This is the default entrypoint to any application written in C. This main is a simple function
+ * that leverages the standard library for basic output to print the sentence "Hello world!"
+ * 
+ * Returns:
+ *
+ *  int
+ */
+ 
 int main() {
-    hello_world();
+
+    printf("Hello, world!!!\n");   
     return 0;
 }
-    
